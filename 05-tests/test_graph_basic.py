fa449c927c59f59f25575eac16adc5b67be6ca87
Veros Kaplan <VerosK@users.noreply.github.com>
2017-10-23 11:09:21 +0200

    update to fixtures

--- 05-tests/test_graph_basic.py
+++ 05-tests/test_graph_basic.py
@@ -2,52 +2,49 @@
 import pytest
 from graph import Graph, GraphException
 
-def test_empty_graph():
-    g = Graph()
-    assert g.node_count() == 0
+@pytest.fixture()
+def graph():
+    return Graph()
 
-def test_node_insert():
-    g = Graph()
-    g.add_node('a')
-    g.add_node('b')
-    g.add_node(42)
-    assert g.node_count() == 3
+def test_empty_graph(graph):
+    assert graph.node_count() == 0
 
-def test_node_duplicate_insert():
-    g = Graph()
-    g.add_node('single')
-    g.add_node('duplicate')
+def test_node_insert(graph):
+    graph.add_node('a')
+    graph.add_node('b')
+    graph.add_node(42)
+    assert graph.node_count() == 3
+
+def test_node_duplicate_insert(graph):
+    graph.add_node('single')
+    graph.add_node('duplicate')
     with pytest.raises(GraphException):
-        g.add_node('duplicate')
+        graph.add_node('duplicate')
 
-def test_edge_insert():
-    g = Graph()
-    g.add_node('A')
-    g.add_node('B')
-    g.add_edge('A', 'B')
+def test_edge_insert(graph):
+    graph.add_node('A')
+    graph.add_node('B')
+    graph.add_edge('A', 'B')
 
 
-def test_edge_invalid():
-    g = Graph()
-    g.add_node('A')
+def test_edge_invalid(graph):
+    graph.add_node('A')
     with pytest.raises(GraphException):
-        g.add_edge('A', 'missing')
+        graph.add_edge('A', 'missing')
 
-def test_edge_insert_duplicate():
-    g = Graph()
+def test_edge_insert_duplicate(graph):
     for i in ['A', 'B', 'C']:
-        g.add_node(i)
-    g.add_edge('A', 'B')
-    g.add_edge('B', 'C')
+        graph.add_node(i)
+    graph.add_edge('A', 'B')
+    graph.add_edge('B', 'C')
     with pytest.raises(GraphException):
-        g.add_edge('A', 'B')
+        graph.add_edge('A', 'B')
 
-def test_edge_is_stored():
-    g = Graph()
+def test_edge_is_stored(graph):
     for i in ['A', 'B', 'C']:
-        g.add_node(i)
-    g.add_edge('A', 'B')
-    g.add_edge('B', 'C')
-    assert g.has_edge('A', 'B') == True
-    assert g.has_edge('B', 'C') == True
-    assert g.has_edge('B', 'A') == False
+        graph.add_node(i)
+    graph.add_edge('A', 'B')
+    graph.add_edge('B', 'C')
+    assert graph.has_edge('A', 'B') == True
+    assert graph.has_edge('B', 'C') == True
+    assert graph.has_edge('B', 'A') == False
